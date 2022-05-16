e9eb5a8138f1d13e5f303a5f2deb30995c8e8bd5
SarahLightBourne <SaraLightBourne@gmail.com>
2022-05-16 15:37:13 +0300

    Init commit

---
+++ users/admin.py
@@ -0,0 +1,9 @@
+from . import models
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+
+@admin.register(models.User)
+class UserAdmin(BaseUserAdmin):
+  pass
