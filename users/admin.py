5305949e3ce8a52d7adcfee17e9a1dcd26b52c47
SarahLightBourne <SaraLightBourne@gmail.com>
2022-05-18 17:32:17 +0300

    Fields

--- users/admin.py
+++ users/admin.py
@@ -6,4 +6,11 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 @admin.register(models.User)
 class UserAdmin(BaseUserAdmin):
-  pass
+  ordering = ('email',)
+  list_display = BaseUserAdmin.list_display[1:]
+
+  fieldsets = (
+    ('Personal Info', {'fields': ('email', 'first_name', 'last_name', 'phone_number', 'whatsapp', 'city', 'website')}),
+    ('Additional Info', {'fields': ('is_lawyer', 'free_consultacy', 'license_date', 'last_login', 'date_joined')}),
+    ('Staff Info', {'fields': ('is_active', 'is_staff', 'is_superuser', 'user_permissions', 'groups'), 'classes': ('collapse',)})
+  )
--- users/models.py
+++ users/models.py
@@ -1,5 +1,28 @@
+from .manager import UserManager
+
+from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+
+phone_number_validator = RegexValidator(r'^(0|90)(\d{10})$')
 
 
 class User(AbstractUser):
-  pass
+  username = None
+  email = models.EmailField('Email', unique=True)
+  phone_number = models.CharField('Phone Number', max_length=12, validators=[phone_number_validator])
+  whatsapp = models.CharField('Whatsapp Number', max_length=12, validators=[phone_number_validator], blank=True)
+
+  is_lawyer = models.BooleanField('Lawyer?', default=False)
+  free_consultacy = models.BooleanField('Free?', default=False)
+  license_date = models.DateTimeField('License Date', blank=True, null=True)
+
+  city = models.CharField('City', max_length=15, blank=True, null=True)
+  website = models.URLField('Website URL', blank=True, null=True)
+
+  USERNAME_FIELD = 'email'
+  REQUIRED_FIELDS = []
+  objects = UserManager()
+
+  def __str__(self) -> None:
+    return self.email
