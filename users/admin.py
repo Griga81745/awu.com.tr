b81b6621754f1f06f5ef36fe9fa8a66687e9cc1a
SarahLightBourne <SaraLightBourne@gmail.com>
2022-05-23 14:07:39 +0300

    Review model

--- users/admin.py
+++ users/admin.py
@@ -13,9 +13,11 @@ class UserAdmin(BaseUserAdmin):
 
   fieldsets = (
     ('Personal Info', {'fields': ('email', 'first_name', 'last_name', 'phone_number', 'whatsapp', 'city', 'website')}),
-    ('Additional Info', {'fields': ('is_lawyer', 'free_consultacy', 'license_date', 'last_login', 'date_joined')}),
+    ('Additional Info', {'fields': ('is_lawyer', 'free_consultacy', 'license_date', 'last_login', 'date_joined', 'rate')}),
     ('Staff Info', {'fields': ('is_active', 'is_staff', 'is_superuser', 'user_permissions', 'groups'), 'classes': ('collapse',)})
   )
+
+  readonly_fields = (*BaseUserAdmin.readonly_fields, 'rate')
 
   add_fieldsets = (
     (None, {
@@ -23,3 +25,16 @@ class UserAdmin(BaseUserAdmin):
       'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')
     }),
   )
+
+
+@admin.register(models.Review)
+class ReviewAdmin(admin.ModelAdmin):
+  search_fields = ('content',)
+  list_display = ('owner', 'destination', 'rate')
+  list_filter = ('rate', 'creation_date')
+
+  fieldsets = (
+    (None, {'fields': ('owner', 'destination', 'content', 'rate', 'creation_date')}),
+  )
+
+  readonly_fields = ('creation_date',)
