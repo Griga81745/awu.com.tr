9f9ce5d7c20f7c82a37fb7ea07d66184eb054491
lamas1901 <ilhanchanel20002@gmail.com>
2022-07-16 17:13:21 +0300

    Temporarily commented sockets

--- settings/asgi.py
+++ settings/asgi.py
@@ -3,18 +3,20 @@
 import os
 from django.core.asgi import get_asgi_application
 
-from .middlewares import TokenAuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 
-from apps.messenger.routing import websocket_urlpatterns as messenger
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.settings'
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.settings')
+# from apps.messenger.routing import websocket_urlpatterns as messenger
+# from .middlewares import TokenAuthMiddlewareStack
+
+
 application = ProtocolTypeRouter({
   'http': get_asgi_application(),
-  'websocket': AllowedHostsOriginValidator(  # Роутер для сокетов из messenger/routing
-    TokenAuthMiddlewareStack(
-      URLRouter(messenger)
-    )
-  )
+  # 'websocket': AllowedHostsOriginValidator(  # Роутер для сокетов из messenger/routing
+  #   TokenAuthMiddlewareStack(
+  #     URLRouter(messenger)
+  #   )
+  # )
 })
