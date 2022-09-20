fabb91237bdab2534c41eb2ba6cd7301dc14056f
lamas1901 <ilhanchanel20002@gmail.com>
2022-09-21 00:20:29 +0300

    fix

--- apps/messenger/routing.py
+++ apps/messenger/routing.py
@@ -1,7 +1,7 @@
-from django.urls import re_path
+from django.conf.urls import url
 
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'^ws/chat/(?P<peer_id>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    url(r'^ws/chat/(?P<peer_id>\w+)/$', consumers.ChatConsumer.as_asgi()),
 ]
